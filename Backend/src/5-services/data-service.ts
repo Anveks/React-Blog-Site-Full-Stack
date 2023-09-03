6fc11f00ca96095c807eb3784121d656d8ecb890
Anveks <vekselmana@gmail.com>
2023-09-03 12:14:54 +0300

    CRUD for comments + add-remove likes added

--- Backend/src/5-services/data-service.ts
+++ Backend/src/5-services/data-service.ts
@@ -1,11 +1,12 @@
 import { OkPacket } from "mysql";
 import { ArticleModel } from "../2-models/articles-model";
 import dal from "../4-utils/dal";
+import { CommentModel } from "../2-models/comment-model";
 
 // get all vacations:
 async function getAllArticles(): Promise<ArticleModel[]>{
   const sql = "SELECT * FROM articles";
-  const articles = dal.execute(sql);
+  const articles = await dal.execute(sql);
   return articles;
 };
 
@@ -24,6 +25,7 @@ async function addArticle(article: ArticleModel): Promise<ArticleModel> {
     article.views,
     article.publicationDate
   ]);
+
   article.articleId = result.insertId;
   return article;
 }
@@ -31,6 +33,7 @@ async function addArticle(article: ArticleModel): Promise<ArticleModel> {
 // edit an article:
 async function updateArticle(article: ArticleModel): Promise<ArticleModel>{
   // TODO: validation
+
   const sql = `UPDATE articles SET 
   authorId = ?,
   categoryId = ?,
@@ -42,6 +45,7 @@ async function updateArticle(article: ArticleModel): Promise<ArticleModel>{
   headImage = ?,
   views = ?,
   publicationDate = ?
+  WHERE articleId = ?
   `;
   const result: OkPacket = await dal.execute(sql, [
     article.authorId,
@@ -53,7 +57,8 @@ async function updateArticle(article: ArticleModel): Promise<ArticleModel>{
     article.previewImage,
     article.headImage,
     article.views,
-    article.publicationDate
+    article.publicationDate,
+    article.articleId
   ])
 
   return article;
@@ -64,9 +69,74 @@ async function deleteArticle(id: number): Promise<void> {
   await dal.execute(sql, [id]);
 }
 
+async function getCommentsPerArticle(id: number): Promise<CommentModel[]> {
+  const sql = `
+SELECT
+  comments.commentId,
+  comments.authorId AS commentAuthorId,
+  comments.articleId,
+  comments.content AS commentContent,
+  comments.commentDate,
+  COUNT(DISTINCT CASE WHEN likes.likeType = 1 THEN likes.userId END) AS likeCount,
+  COUNT(DISTINCT CASE WHEN likes.likeType = 0 THEN likes.userId END) AS dislikeCount
+FROM
+  comments
+LEFT JOIN
+  likes ON comments.commentId = likes.commentId
+WHERE
+  comments.articleId = ?
+GROUP BY
+  comments.commentId,
+  comments.authorId,
+  comments.articleId,
+  comments.content,
+  comments.commentDate;
+  `;
+  const comments = await dal.execute(sql, [id]);
+  return comments;
+}
+
+async function addComment(comment: CommentModel): Promise<CommentModel>{
+  const sql = `INSERT INTO comments VALUES (DEFAULT, ?, ?, ?, ?)`;
+  const result: OkPacket = await dal.execute(sql, [comment.authorId, comment.articleId, comment.content, comment.commentDate]);
+  comment.commentId = result.insertId;
+  return comment;
+}
+
+async function updateComment(comment: CommentModel): Promise<CommentModel>{
+  const sql = `
+  UPDATE comments
+  SET content = ?
+  WHERE commentId = ?;
+  `;
+  const result: OkPacket = await dal.execute(sql, [comment.content, comment.commentId]);
+  return comment;
+}
+
+async function deleteComment(id: number): Promise<void> {
+  const sql = "DELETE FROM comments WHERE commentId = ?";
+  await dal.execute(sql, [id]);
+}
+
+async function addLike(userId: number, commentId: number, likeType: number): Promise<void> {
+  const sql = "INSERT INTO likes VALUES(?,?,?)";
+  await dal.execute(sql, [likeType, userId, commentId]);
+}
+
+async function removeLike(userId: number, commentId: number): Promise<void>{
+  const sql = "DELETE FROM likes WHERE userId = ? AND commentId = ?";
+  await dal.execute(sql, [userId, commentId]);
+}
+
 export default {
   getAllArticles,
   addArticle,
   updateArticle,
-  deleteArticle
+  deleteArticle,
+  getCommentsPerArticle,
+  addComment,
+  updateComment,
+  deleteComment,
+  addLike,
+  removeLike
 };
--- Backend/src/6-routes/data-routes.ts
+++ Backend/src/6-routes/data-routes.ts
@@ -1,6 +1,7 @@
 import express, { NextFunction, Request, Response, request } from "express";
 import dataService from "../5-services/data-service";
 import { ArticleModel } from "../2-models/articles-model";
+import { CommentModel } from "../2-models/comment-model";
 
 const router = express.Router();
 
@@ -52,7 +53,74 @@ router.delete("/articles/:id([0-9]+)", async (request: Request, response: Respon
     }
 });
 
+// GET comments
+router.get("/comments/:id([0-9]+)", async (request: Request, response: Response, next: NextFunction) => {
+    try {
+        const comments = await dataService.getCommentsPerArticle(+request.params.id);
+        response.json(comments);
+    }
+    catch(err: any) {
+        next(err);        
+    }
+});
 
+// ADD comment
+router.post("/comments", async (request: Request, response: Response, next: NextFunction) => {
+    try {
+        const comment = new CommentModel(request.body);
+        const newComment = await dataService.addComment(comment);
+        response.status(201).json(newComment);
+    }
+    catch(err: any) {
+        next(err);        
+    }
+});
+
+// UPDATE COMMENT
+router.put("/update-comments", async (request: Request, response: Response, next: NextFunction) => {
+    try {
+        const comment = new CommentModel(request.body);
+        const updatedComment = await dataService.updateComment(comment);
+        response.json(updatedComment);
+    }
+    catch(err: any) {
+        next(err);        
+    }
+});
+
+router.delete("/delete-comment", async (request: Request, response: Response, next: NextFunction) => {
+    try {
+        const id = +request.body.commentId;
+        await dataService.deleteComment(id);
+        response.sendStatus(204);
+    }
+    catch(err: any) {
+        next(err);        
+    }
+});
+
+// ADD like/dislike
+router.post("/add-likes", async (request: Request, response: Response, next: NextFunction) => {
+    try {
+        const { userId, commentId, likeType } = request.body;
+        await dataService.addLike(+userId, +commentId, +likeType);
+        response.status(204).send("Like has been added.");
+    }
+    catch(err: any) {
+        next(err);        
+    }
+});
+
+router.delete("/delete-likes", async (request: Request, response: Response, next: NextFunction) => {
+    try {
+        const { userId, commentId } = request.body;
+        await dataService.removeLike(+userId, +commentId);
+        response.sendStatus(204);
+    }
+    catch(err: any) {
+        next(err);        
+    }
+});
 
 export default router;
 
