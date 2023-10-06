1b8072d70dfe6f975440220a8efce3406a68a1a7
Anveks <vekselmana@gmail.com>
2023-10-06 14:05:14 +0300

    slider logic & functions added

--- Frontend/src/Components/UI/Slider/Slider.tsx
+++ Frontend/src/Components/UI/Slider/Slider.tsx
@@ -1,13 +1,41 @@
+import { useState } from "react";
 import "./Slider.css";
+import { ArticleModel } from "../../../Models/ArticleModel";
 
 function Slider(props: any): JSX.Element {
+
+    let indexArr = props.articles.map((article: ArticleModel) => { return article.articleId });
+
+    const [current, setCurrent] = useState(0);
+
+    const slideLeft = () => {
+        if (current === 0) {
+            setCurrent(indexArr.length - 1);
+        } else {
+            setCurrent(current - 1);
+        }
+    }
+
+    const slideRight = () => {
+        if (current === indexArr.length - 1) {
+            setCurrent(0)
+        } else {
+            setCurrent(current + 1)
+        }
+    }
+
 
     return (
         <div className="Slider">
 
             <div className="slider-wrapper">
                 {props.articles.map((article: any) =>
-                    <div className="slider-card" key={article.articleId}>
+                    <div
+                        className={
+                            article.articleId === indexArr[current]
+                                ? "slider-card slider-card-active"
+                                : "slider-card"}
+                        key={article.articleId}>
 
                         <img className="slider-image" src={article.previewImageUrl} alt="slider-image" />
 
@@ -16,6 +44,16 @@ function Slider(props: any): JSX.Element {
                         </div>
 
                     </div>)}
+
+                {/* left arrow */}
+                <div className="slider-arrow-left" onClick={slideLeft}>
+                    &lsaquo;
+                </div>
+                {/* right arrow */}
+                <div className="slider-arrow-right" onClick={slideRight}>
+                    &rsaquo;
+                </div>
+
             </div>
 
         </div>
