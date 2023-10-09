663a2660f1ecbc2c1f7e19eb3213a7dd0a935872
Anveks <vekselmana@gmail.com>
2023-10-09 17:38:51 +0300

    .

--- Frontend/src/Components/UI/Slider/Slider.tsx
+++ Frontend/src/Components/UI/Slider/Slider.tsx
@@ -3,7 +3,6 @@ import "./Slider.css";
 import { ArticleModel } from "../../../Models/ArticleModel";
 
 function Slider(props: any): JSX.Element {
-
     let indexArr = props.articles.map((article: ArticleModel) => { return article.articleId });
 
     const [current, setCurrent] = useState(0);
@@ -22,50 +21,57 @@ function Slider(props: any): JSX.Element {
         );
     }
 
+    const handlePaginationClick = (index: number) => {
+        console.log(`Pagination dot clicked: ${index}`);
+        setCurrent(index);
+    }
 
     return (
         <div className="Slider">
-
             <div className="slider-wrapper">
-                {props.articles.map((article: any) =>
+                {props.articles.map((article: any, index: number) => (
                     <div
                         className={
                             article.articleId === indexArr[current]
                                 ? "slider-card slider-card-active"
-                                : "slider-card"}
-                        key={article.articleId}>
-
+                                : "slider-card"
+                        }
+                        key={article.articleId}
+                    >
                         <img className="slider-image" src={article.previewImageUrl} alt="slider-image" />
-
                         <div className="card-overlay">
                             <h2 className="card-title">{article.title}</h2>
                         </div>
-
-                    </div>)}
+                    </div>
+                ))}
 
                 {/* left arrow */}
                 <div className="slider-arrow-left" onClick={slideLeft}>
                     &lsaquo;
                 </div>
+
                 {/* right arrow */}
                 <div className="slider-arrow-right" onClick={slideRight}>
                     &rsaquo;
                 </div>
 
                 <div className="slider-pagination">
-                    {indexArr.map((index: number) => <div key={index} className={
-                        index === indexArr[current]
-                            ? "pagination-dot pagination-dot-active"
-                            : "pagination-dot"}
-
-                        onClick={() => { setCurrent(index) }}
-                    ></div>)}
+                    {indexArr.map((index: number) => (
+                        <div
+                            key={index}
+                            className={
+                                index === indexArr[current]
+                                    ? "pagination-dot pagination-dot-active"
+                                    : "pagination-dot"
+                            }
+                            onClick={() => setCurrent(index)}
+                        ></div>
+                    ))}
                 </div>
-
             </div>
-
         </div>
     );
 }
 
 export default Slider;
+
